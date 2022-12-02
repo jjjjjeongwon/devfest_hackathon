ea0e77dced820ec989eebac1e9d5e95d5ccf315f
letsjo <hyunoh.jo@gmail.com>
2022-12-03 04:59:26 +0900

    feat(*): 머지 후 메인 페이지 일부 수정

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from "react";
-import styled from "styled-components";
-import { Route, Routes } from "react-router-dom";
+import React from 'react';
+import styled from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
 
-import SelectPage from "./SelectPage";
-import Login from "./Login";
-import Signup from "./Signup";
-import Navbar from "./components/Navbar";
+import SelectPage from './SelectPage';
+import Login from './Login';
+import Signup from './Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Container>
-    <Routes>
-    <Route path="/" element={<Login/>} />
-    <Route path="/signup" element={<Signup/>} />
-    <Route path="/selectpage" element={<SelectPage />} />
-    </Routes>
-    
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/selectpage" element={<SelectPage />} />
+      </Routes>
     </Container>
-  )
+  );
 }
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-bottom: 50px;
-  background-color:darkseagreen;
-  height: 100vh;
+  padding-bottom: 50px;
+  background-color: darkseagreen;
+  height: 100%;
 `;
 
 export default App;
--- src/Login.js
+++ src/Login.js
@@ -1,111 +1,95 @@
-import React from 'react'
-import {auth, db} from "./shared/firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import {getDocs, where, query, collection} from "firebase/firestore";
-import styled from "styled-components";
-import {useNavigate} from "react-router-dom";
+import React from 'react';
+import { auth, db } from './shared/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { getDocs, where, query, collection } from 'firebase/firestore';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 
-
-
 const Login = () => {
-    const id_ref =React.useRef(null);
-    const pw_ref = React.useRef(null);
-    const navigate = useNavigate();
-    const loginFB =async() =>{
-        console.log(id_ref.current.value, pw_ref.current.value);
-        if(id_ref.current.value === "" || pw_ref.current.value ===""){
-          alert("아이디, 비밀번호를 입력해주세요!");
-        }else{
-          try{
-            const user = await signInWithEmailAndPassword(auth,
-              id_ref.current.value, 
-              pw_ref.current.value
-              );
-              console.log(user.user.email);
-             
-  
-              
-                alert("로그인 완료!");
-                navigate("/selectpage");
-                // window.location.replace("/");
-          }catch(e){
-            alert("로그인 정보를 확인해주세요!")
-          }
-          
-        };
+  const id_ref = React.useRef(null);
+  const pw_ref = React.useRef(null);
+  const navigate = useNavigate();
+  const loginFB = async () => {
+    console.log(id_ref.current.value, pw_ref.current.value);
+    if (id_ref.current.value === '' || pw_ref.current.value === '') {
+      alert('아이디, 비밀번호를 입력해주세요!');
+    } else {
+      try {
+        const user = await signInWithEmailAndPassword(
+          auth,
+          id_ref.current.value,
+          pw_ref.current.value,
+        );
+        console.log(user.user.email);
 
-        
-            //  navigate("/");
-            //  alert("로그인 완료!");
-            // console.log(id_ref.current.value);
-            
-
-             
-               
+        alert('로그인 완료!');
+        navigate('/selectpage');
+        // window.location.replace("/");
+      } catch (e) {
+        alert('로그인 정보를 확인해주세요!');
+      }
     }
 
-    // const navigator =useNavigate();
-    // useEffect(()=> {
-    //   if(is_login){
-    //     navigator("/")
-    //   }
-    // })
-    
+    //  navigate("/");
+    //  alert("로그인 완료!");
+    // console.log(id_ref.current.value);
+  };
 
-
+  // const navigator =useNavigate();
+  // useEffect(()=> {
+  //   if(is_login){
+  //     navigator("/")
+  //   }
+  // })
 
   return (
     <Wrap>
-        <Logo href="/">
-          <img src="/images/KakaoTalk_Photo_2022-12-03-03-39-38.png" alt="" />
-        </Logo>
-        <LoginText>
-            로그인
-        </LoginText>
-    <InputBox>
-    <Text>
-        아이디(이메일)
-        </Text>
-         <Id type="text" ref={id_ref}  /> 
-        </InputBox>
-        <InputBox>
-        <Text>
-        비밀번호 
-        </Text>
-         <Id ref={pw_ref} type="password"/> <br/><br/>
-         </InputBox>
-         <ButtonContainer>
-        <Button onClick={loginFB} >로그인</Button>
-        <SignupButton onClick={() => {
-                navigate("/signup");
-              }} >아직 회원이 아니신가요?</SignupButton>
-        </ButtonContainer>
+      <Logo href="/">
+        <img src="/images/KakaoTalk_Photo_2022-12-03-03-39-38.png" alt="" />
+      </Logo>
+      <LoginText>로그인</LoginText>
+      <InputBox>
+        <Text>아이디(이메일)</Text>
+        <Id type="text" ref={id_ref} />
+      </InputBox>
+      <InputBox>
+        <Text>비밀번호</Text>
+        <Id ref={pw_ref} type="password" /> <br />
+        <br />
+      </InputBox>
+      <ButtonContainer>
+        <Button onClick={loginFB}>로그인</Button>
+        <SignupButton
+          onClick={() => {
+            navigate('/signup');
+          }}
+        >
+          아직 회원이 아니신가요?
+        </SignupButton>
+      </ButtonContainer>
     </Wrap>
-
-  )
-}
-
+  );
+};
 
 const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+  margin: 80px auto;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 50%;
-    height : 100%;
-    margin: 80px auto;
-
-    border-radius: 10px;
-    padding-top: 20px;
-    padding-bottom: 5%;
-    background-color: white;
+  border-radius: 10px;
+  padding-top: 20px;
+  padding-bottom: 5%;
+  background-color: white;
 `;
 
 const Logo = styled.a`
   cursor: pointer;
-  display: flex; 
+  display: flex;
   justify-content: center;
   align-items: center;
   width: 100px;
@@ -119,58 +103,57 @@ const Logo = styled.a`
   }
 `;
 
-const LoginText =styled.div`
-margin: 0px auto 20px;
-    font-size: 25px;
-    font-weight: bold;
+const LoginText = styled.div`
+  margin: 0px auto 20px;
+  font-size: 25px;
+  font-weight: bold;
 `;
-const InputBox =styled.div`
-     width : 60%;
-`;
-
-const Text =styled.div`
-    font-size: 15px;
-    font-weight: bold;
+const InputBox = styled.div`
+  width: 60%;
 `;
 
-const Id =styled.input`
-width: 97%;
-    height: 25px;
-    margin-bottom: 15px;
-    padding: 5px 5px;
-    border-radius: 3px;
-    border-bottom: 2px solid rgb(219, 232, 216);
-    border : 1px solid lightslategray;
-    font-size: 12px;
-    font-weight: 500;
-    
+const Text = styled.div`
+  font-size: 15px;
+  font-weight: bold;
 `;
 
-const ButtonContainer =styled.div`
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    flex-direction: column;
-    width: 60%;
+const Id = styled.input`
+  width: 97%;
+  height: 25px;
+  margin-bottom: 15px;
+  padding: 5px 5px;
+  border-radius: 3px;
+  border-bottom: 2px solid rgb(219, 232, 216);
+  border: 1px solid lightslategray;
+  font-size: 12px;
+  font-weight: 500;
 `;
 
-const Button =styled.button`
-    width: 100%;
-    height: 30px;
-    margin-bottom: 10px;
-    font-size: 15px;
-    border: none;
-    cursor: pointer;
-    background-color: seagreen;
-    color:white;
-    font-weight: bolder;
-    border-radius: 3px;
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  width: 60%;
 `;
 
-const SignupButton =styled.div`
-    font-size: 10px;
-    cursor: pointer;
-    &:hover {
+const Button = styled.button`
+  width: 100%;
+  height: 30px;
+  margin-bottom: 10px;
+  font-size: 15px;
+  border: none;
+  cursor: pointer;
+  background-color: seagreen;
+  color: white;
+  font-weight: bolder;
+  border-radius: 3px;
+`;
+
+const SignupButton = styled.div`
+  font-size: 10px;
+  cursor: pointer;
+  &:hover {
     font-weight: bold;
   }
 `;
